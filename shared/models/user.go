9cf58d84b3da4cdfe5f11d77f543eef0fa535d89
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-22 16:36:52 -0300

    Add user validation

--- shared/models/user.go
+++ shared/models/user.go
@@ -1,11 +1,11 @@
 package models
 
 type User struct {
-	Id       int    `json:"id"`
-	Role     int    `json:"role"`
-	TeamsIds []int  `json:"teamsIds"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
+	Id       int    `json:"id" validate:"-"`
+	Role     int    `json:"role" validate:"required"`
+	TeamsIds []int  `json:"teamsIds" validate:"required"`
+	Name     string `json:"name" validate:"required,min=2,max=50"`
+	Username string `json:"username" validate:"required,min=3,max=50"`
+	Password string `json:"password" validate:"required,min=5,max=50"`
+	Email    string `json:"email" validate:"required,email"`
 }
