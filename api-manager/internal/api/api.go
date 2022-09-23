59ab29a51495862c0c6685564ea7f08c7b528283
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-23 13:39:00 -0300

    Handle pg connection

--- api-manager/internal/api/api.go
+++ api-manager/internal/api/api.go
@@ -1,6 +1,11 @@
 package api
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/fernandotsda/nemesys/shared/db"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -17,8 +22,15 @@ type API struct {
 	Validate *validator.Validate
 }
 
-// Create a new API instance
-func New(pgConn *db.PgConn) (*API, error) {
+// Create a new API instance.
+func New() (*API, error) {
+	// connect to postgresql
+	pgConn, err := db.ConnectToPG(os.Getenv("PG_URL"))
+	if err != nil {
+		return nil, fmt.Errorf("fail to connect to pg, err: %s", err)
+	}
+	log.Println("connected to postgresql")
+
 	// create gin engine
 	r := gin.New()
 
@@ -35,4 +47,10 @@ func New(pgConn *db.PgConn) (*API, error) {
 // Start listen and server
 func (api *API) Run(addr string) error {
 	return api.Router.Run(addr)
+}
+
+// Close all api dependencies. It's safe to call Close
+// on a already closed one
+func (api *API) Close() {
+	api.PgConn.Close(context.Background())
 }
