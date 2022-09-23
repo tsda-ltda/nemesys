15c6cadbcd9b3bd92d5268a8308cd82b3fb81544
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-23 08:13:39 -0300

    Add connect to pg function

---
+++ shared/db/connect.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+// Connects to a Postgresql database server
+func ConnectToPG(url string) (*PgConn, error) {
+	// connect to pg db
+	conn, err := pgx.Connect(context.Background(), url)
+	if err != nil {
+		return nil, err
+	}
+
+	// ping db
+	err = conn.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return &PgConn{
+		conn,
+	}, nil
+}
