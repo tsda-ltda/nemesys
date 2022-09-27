4e6692f02d12472fbceb0cc32f02136d86eca7c6
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-27 09:43:52 -0300

    Remove team id from users asynchronously

--- api-manager/internal/team/delete.go
+++ api-manager/internal/team/delete.go
@@ -3,7 +3,6 @@ package team
 import (
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/fernandotsda/nemesys/api-manager/internal/api"
 	"github.com/gin-gonic/gin"
@@ -11,37 +10,48 @@ import (
 
 // Deletes team from databse
 // Responses:
-//   - 400 If invalid id
 //   - 404 If team not founded
 //   - 204 If succeeded
 func DeleteHandler(api *api.API) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		// get id from param
-		id, err := strconv.Atoi(c.Param("id"))
+		// get id
+		id, err := getId(api, c)
 		if err != nil {
-			c.Status(http.StatusBadRequest)
-			return
-		}
-
-		// check if team exists
-		e, err := api.PgConn.Teams.Exists(c.Request.Context(), id)
-		if err != nil {
-			c.Status(http.StatusInternalServerError)
-			log.Printf("\nfail to query team, err: %s", err)
-			return
-		}
-		if !e {
 			c.Status(http.StatusNotFound)
 			return
 		}
 
+		// get users ids
+		sql := `SELECT users_ids FROM teams WHERE id = $1`
+		rows, err := api.PgConn.Query(c.Request.Context(), sql, id)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			log.Printf("\nfail to query team id and users ids, err: %s", err)
+			return
+		}
+
+		// scan results
+		var usersIds []int
+		for rows.Next() {
+			rows.Scan(&usersIds)
+		}
+
+		// check if team doesn't exists
+		if rows.CommandTag().RowsAffected() == 0 {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		// delete team
 		_, err = api.PgConn.Teams.Delete(c.Request.Context(), id)
 		if err != nil {
 			c.Status(http.StatusInternalServerError)
 			log.Printf("\nfail to delete team, err: %s", err)
 			return
 		}
-		log.Printf("\tteam id %d deleted successfully", id)
+
+		// remove team id from users
+		go updateUsersTeamsIds(usersIds, []int{}, id, api, c.Request.Context())
 
 		c.Status(http.StatusNoContent)
 	}
