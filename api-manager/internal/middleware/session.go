2be5647a8b5d850f81e356bf3d8795df17dd3017
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-29 13:39:13 -0300

    Fix import

--- api-manager/internal/middleware/session.go
+++ api-manager/internal/middleware/session.go
@@ -3,13 +3,13 @@ package middleware
 import (
 	"github.com/fernandotsda/nemesys/api-manager/internal/api"
 	"github.com/fernandotsda/nemesys/api-manager/internal/auth"
-	"github.com/fernandotsda/nemesys/api-manager/internal/user"
+	"github.com/fernandotsda/nemesys/api-manager/internal/uauth"
 	"github.com/gin-gonic/gin"
 )
 
 func validateSession(api *api.API, c *gin.Context) (meta auth.SessionMeta, err error) {
 	// get session cookie
-	sess, err := c.Cookie(user.SessionCookieName)
+	sess, err := c.Cookie(uauth.SessionCookieName)
 	if err != nil {
 		return meta, err
 	}
