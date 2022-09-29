ddb50d8d50a8746e9de625bee434bff9baca4ece
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-29 10:13:24 -0300

    Add Auth struct to handle authentications

---
+++ api-manager/internal/auth/auth.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/fernandotsda/nemesys/shared/env"
+	"github.com/go-redis/redis/v8"
+)
+
+type Auth struct {
+	// Redis database client.
+	rdb *redis.Client
+
+	// Session Time To Live
+	SessionTTL time.Duration
+
+	// Session Token size
+	TokenSize int
+}
+
+type AuthConfig struct {
+}
+
+// Creates a new authentication handler
+func New(rdb *redis.Client) (*Auth, error) {
+	// parse UserSessionTTL to int
+	ttl, err := strconv.Atoi(env.UserSessionTTL)
+	if err != nil {
+		return nil, fmt.Errorf("fail to parse env.UserSessionTTL to int, err:%s", err)
+	}
+
+	// parse UserSessionTokenSize to int
+	size, err := strconv.Atoi(env.UserSessionTokenSize)
+	if err != nil {
+		return nil, fmt.Errorf("fail to parse env.UserSessionTokenSize to int, err:%s", err)
+	}
+
+	return &Auth{
+		rdb:        rdb,
+		SessionTTL: time.Second * time.Duration(ttl),
+		TokenSize:  size,
+	}, nil
+}
+
+func (a *Auth) Close() error {
+	return a.rdb.Close()
+}
