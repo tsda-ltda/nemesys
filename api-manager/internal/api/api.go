55879620d68360f5b473d841a13bb04d11881101
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-29 10:19:59 -0300

    Use env info and internal db pkg

--- api-manager/internal/api/api.go
+++ api-manager/internal/api/api.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
+	"github.com/fernandotsda/nemesys/api-manager/internal/auth"
+	_db "github.com/fernandotsda/nemesys/api-manager/internal/db"
 	"github.com/fernandotsda/nemesys/shared/db"
+	"github.com/fernandotsda/nemesys/shared/env"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
@@ -18,20 +20,40 @@ type API struct {
 	// Gin web fremework engine
 	Router *gin.Engine
 
+	// Auth handler
+	Auth *auth.Auth
+
 	// Validator
 	Validate *validator.Validate
+
+	// User pw hash cost
+	UserPWBcryptCost int
 }
 
 // Create a new API instance.
 func New() (*API, error) {
 	// connect to postgresql
-	pgConn, err := db.ConnectToPG(os.Getenv("PG_URL"))
+	pgConn, err := _db.PGConnectAndInit()
 	if err != nil {
-		return nil, fmt.Errorf("fail to connect to pg, err: %s", err)
+		return nil, err
 	}
 	log.Println("connected to postgresql")
 
+	// connect to redis auth
+	rdbAuth, err := _db.RDBAuthConnectAndInit()
+	if err != nil {
+		return nil, err
+	}
+	log.Println("connected to redis")
+
+	// create authentication handler
+	auth, err := auth.New(rdbAuth)
+	if err != nil {
+		return nil, fmt.Errorf("fail to create auth handler, err: %s", err)
+	}
+
 	// create gin engine
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 
 	// create validator
@@ -40,17 +62,21 @@ func New() (*API, error) {
 	return &API{
 		PgConn:   pgConn,
 		Router:   r,
+		Auth:     auth,
 		Validate: validate,
 	}, nil
 }
 
 // Start listen and server
-func (api *API) Run(addr string) error {
-	return api.Router.Run(addr)
+func (api *API) Run() error {
+	url := fmt.Sprintf("%s:%s", env.APIManagerHost, env.APIManagerPort)
+	log.Printf("server listen to: %s", url)
+	return api.Router.Run(url)
 }
 
 // Close all api dependencies. It's safe to call Close
 // on a already closed one
 func (api *API) Close() {
 	api.PgConn.Close(context.Background())
+	api.Auth.Close()
 }
