f7aea07a57ebd1d01af577d15bf664288b1967b4
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-09-29 10:15:15 -0300

    Remove unused functions

--- shared/db/pg_team.go
+++ shared/db/pg_team.go
@@ -4,23 +4,10 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type TeamsORM struct {
 	conn *pgx.Conn
-}
-
-// Creates the teams table
-func (u *TeamsORM) CreateTable(ctx context.Context) (pgconn.CommandTag, error) {
-	sql := `CREATE TABLE teams (
-		id serial4 PRIMARY KEY,
-		name VARCHAR (50) NOT NULL,
-		ident VARCHAR (50) UNIQUE NOT NULL,
-		descr VARCHAR (255) NOT NULL,
-		users_ids INT[] NOT NULL
-	)`
-	return u.conn.Exec(ctx, sql)
 }
 
 // Check by id if team exists
--- shared/db/pg_user.go
+++ shared/db/pg_user.go
@@ -4,25 +4,10 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type UsersORM struct {
 	conn *pgx.Conn
-}
-
-// Creates the users table
-func (u *UsersORM) CreateTable(ctx context.Context) (pgconn.CommandTag, error) {
-	sql := `CREATE TABLE users (
-		id serial4 PRIMARY KEY,
-		name VARCHAR (50) NOT NULL,
-		username VARCHAR (50) UNIQUE NOT NULL,
-		password VARCHAR (255) NOT NULL,
-		email VARCHAR (255) UNIQUE NOT NULL,
-		role INT2 NOT NULL,
-		teams_ids INT[]
-	)`
-	return u.conn.Exec(ctx, sql)
 }
 
 // Check by id if user exists
