fd42588ec849d76a88724905ab8022dd5bfa24bb
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-10-20 08:10:51 -0300

    Add amqp Queues and Exchanges

---
+++ shared/amqp/queues.go
@@ -0,0 +1,8 @@
+package amqp
+
+const (
+	QueueSNMPConnRegistRequired = "services.snmp.conn_regist_required"
+	ExchangeSNMPGet             = "services.snmp.get"
+	ExchangeSNMPData            = "services.snmp.data"
+	ExchangeSNMPConnRegister    = "services.snmp.conn_regist"
+)
