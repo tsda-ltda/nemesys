5bf048704f83a4ae27957c61d55ee1e1737d9b7d
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-10-25 17:44:08 -0300

    fix(middleware): Wrong import path

--- api-manager/internal/middleware/session.go
+++ api-manager/internal/middleware/session.go
@@ -3,13 +3,12 @@ package middleware
 import (
 	"github.com/fernandotsda/nemesys/api-manager/internal/api"
 	"github.com/fernandotsda/nemesys/api-manager/internal/auth"
-	"github.com/fernandotsda/nemesys/api-manager/internal/uauth"
 	"github.com/gin-gonic/gin"
 )
 
 func validateSession(api *api.API, c *gin.Context) (meta auth.SessionMeta, err error) {
 	// get session cookie
-	sess, err := c.Cookie(uauth.SessionCookieName)
+	sess, err := c.Cookie(auth.SessionCookieName)
 	if err != nil {
 		return meta, err
 	}
