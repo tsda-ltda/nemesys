9a1725ed8d44e147a69ab8217f89670f69105fb7
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-10-25 17:36:55 -0300

    ref(models): Rework models

--- shared/models/snmp.go
+++ shared/models/snmp.go
@@ -1,56 +1,43 @@
 package models
 
-import (
-	"time"
-)
+type SNMPContainer struct {
+	// ContainerId is the container id.
+	ContainerId int `json:"container-id" validate:"-"`
 
-type SNMPAgentConfig struct {
-	// TTL is the Time to live of this configuration on the SNMP service memory.
-	TTL time.Duration
+	// CacheDuration is the cache duration in miliseconds of this configuration on the SNMP service.
+	CacheDuration int `json:"cache-duration" validate:"required"`
 
 	// Target is an ipv4 address.
-	Target string
+	Target string `json:"target" validate:"required,max=50"`
 
 	// Port is a port.
-	Port uint16
+	Port uint16 `json:"port" validate:"required,max=65535"`
 
 	// Transport is the transport protocol to use ("udp" or "tcp"); if unset "udp" will be used.
-	Transport string
+	Transport string `json:"transport" validate:"required,max=3"`
 
 	// Community is an SNMP Community string.
-	Community string
+	Community string `json:"community" validate:"required,max=50"`
 
 	// Timeout is the timeout for one SNMP request/response.
-	Timeout time.Duration
+	Timeout int `json:"timeout" validate:"required,max=60000"`
 
 	// Set the number of retries to attempt.
-	Retries int
-
-	// Double timeout in each retry.
-	ExponentialTimeout bool
+	Retries int `json:"retries" validate:"required"`
 
 	// MsgFlags is an SNMPV3 MsgFlags.
-	MsgFlags uint8
+	MsgFlags uint8 `json:"msg-flags" validate:"-"`
 
 	// Version is an SNMP Version.
-	Version uint8
+	Version uint8 `json:"version" validate:"min=0,max=3"`
 
 	// Max oids per request.
-	MaxOidsPerReq int
+	MaxOids int `json:"max-oids" validate:"required"`
 }
 
-type SNMPGetMetrics struct {
-	// OIDS are the metrics object identifier.
-	OIDS []string
-
-	// Target is a ipv4 address.
-	Target string
-
-	// Port is the port.
-	Port uint16
-}
-
-type SNMPGetMetricResult struct {
-	OID   string
-	Value any
+type SNMPMetric struct {
+	// MetricId is the metric identifier.
+	MetricId int `json:"-" validate:"-"`
+	// OID is the snmp object identifier.
+	OID string `json:"oid" validate:"required,max=128"`
 }
--- shared/models/user.go
+++ shared/models/user.go
@@ -1,24 +1,38 @@
 package models
 
 type User struct {
-	Id       int    `json:"id" validate:"-"`
-	Role     int    `json:"role" validate:"required"`
-	Name     string `json:"name" validate:"required,min=2,max=50"`
+	// Id is the user identifier.
+	Id int `json:"id" validate:"-"`
+	// Role is the user role.
+	Role uint8 `json:"role" validate:"required"`
+	// Name is the user name.
+	Name string `json:"name" validate:"required,min=2,max=50"`
+	// Username is the user's username.
 	Username string `json:"username" validate:"required,min=3,max=50"`
+	// Password is the user's password.
 	Password string `json:"password" validate:"required,min=5,max=50"`
-	Email    string `json:"email" validate:"required,email"`
+	// Email is the user's email.
+	Email string `json:"email" validate:"required,email"`
 }
 
 type UserSimplified struct {
-	Id       int    `json:"id" validate:"-"`
+	// Id is the user identifier.
+	Id int `json:"id" validate:"-"`
+	// Username is the user's username.
 	Username string `json:"username" validate:"required,min=3,max=50"`
-	Name     string `json:"name" validate:"required,min=2,max=50"`
+	// Name is the user name.
+	Name string `json:"name" validate:"required,min=2,max=50"`
 }
 
 type UserWithoutPW struct {
-	Id       int    `json:"id" validate:"-"`
-	Role     int    `json:"role" validate:"required"`
-	Name     string `json:"name" validate:"required,min=2,max=50"`
+	// Id is the user identifier.
+	Id int `json:"id" validate:"-"`
+	// Role is the user role.
+	Role int `json:"role" validate:"required"`
+	// Name is the user name.
+	Name string `json:"name" validate:"required,min=2,max=50"`
+	// Username is the user's username.
 	Username string `json:"username" validate:"required,min=3,max=50"`
-	Email    string `json:"email" validate:"required,email"`
+	// Email is the user's email.
+	Email string `json:"email" validate:"required,email"`
 }
