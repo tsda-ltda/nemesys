3929dc92fa9e3a6827f2ee9b71bfbb37bbab40eb
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-11-04 11:52:25 -0300

    feat(ev): Add evaluator package

---
+++ shared/evaluator/evaluator.go
@@ -0,0 +1,83 @@
+package evaluator
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Knetic/govaluate"
+	"github.com/fernandotsda/nemesys/shared/cache"
+	"github.com/fernandotsda/nemesys/shared/db"
+	"github.com/fernandotsda/nemesys/shared/types"
+)
+
+type Evaluator struct {
+	pgConn *db.PgConn
+	cache  *cache.Cache
+}
+
+func New(pgConn *db.PgConn) *Evaluator {
+	return &Evaluator{
+		pgConn: pgConn,
+		cache:  cache.New(),
+	}
+}
+
+func (e *Evaluator) Evaluate(v any, metricId int64, mt types.MetricType) (any, error) {
+	ctx := context.Background()
+
+	// get on cache
+	exists, evexp, err := e.cache.GetMetricEvExpression(ctx, metricId)
+	if err != nil {
+		return nil, err
+	}
+
+	// check if exists
+	if !exists {
+		// get on database
+		exists, evexp, err = e.pgConn.Metrics.GetEvaluableExpression(ctx, metricId)
+		if err != nil {
+			return nil, err
+		}
+
+		// check if exists
+		if !exists {
+			return nil, errors.New("fail to get evaluable expression, metric does not exists")
+		}
+	}
+
+	// save / reset cache
+	err = e.cache.SetMetricEvExpression(ctx, metricId, evexp)
+	if err != nil {
+		return nil, err
+	}
+
+	// check if is empty
+	if evexp == "" {
+		return v, nil
+	}
+
+	// get expression struct
+	exp, err := govaluate.NewEvaluableExpression(evexp)
+	if err != nil {
+		return nil, err
+	}
+
+	// set params
+	params := make(map[string]any, 1)
+	params["x"] = v
+
+	// evaluate
+	r, err := exp.Evaluate(params)
+	if err != nil {
+		return nil, err
+	}
+
+	// parse value
+	parsed, err := types.ParseValue(r, mt)
+	if err != nil {
+		return nil, err
+	}
+
+	return parsed, nil
+
+}
