f663763c52e7dea5439ead0b8e6e6a16b67ff38c
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-11-10 18:01:53 -0300

    fear(amqp): Add exchanges

---
+++ shared/amqp/channel.go
@@ -0,0 +1,11 @@
+package amqp
+
+import "github.com/rabbitmq/amqp091-go"
+
+func OnChannelClose(ch *amqp091.Channel) (closed chan *amqp091.Error, canceled chan string) {
+	closed = make(chan *amqp091.Error)
+	canceled = make(chan string)
+	ch.NotifyCancel(canceled)
+	ch.NotifyClose(closed)
+	return closed, canceled
+}
