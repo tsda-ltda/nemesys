6ecb4d814f3c194882fc1e6c9eacc088e572cff1
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-11-18 08:09:05 -0300

    feat(amqp): Add default expiration for messages

--- shared/amqp/dial.go
+++ shared/amqp/dial.go
@@ -7,6 +7,9 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// DefaultExp is the default expiration in milliseconds.
+const DefaultExp = "5000"
+
 // Use current enviroment to connect to amqp server.
 func Dial() (*amqp.Connection, error) {
 	url := fmt.Sprintf("amqp://%s:%s@%s:%s/", env.AMQPUsername, env.AMQPPassword, env.AMQPHost, env.AMQPPort)
