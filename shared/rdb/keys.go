d5a480ff7b23d5c42a3e36c934e0942f5aa406eb
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-11-18 08:12:26 -0300

    feat(rdb): Add cache key for data-policy

--- shared/rdb/keys.go
+++ shared/rdb/keys.go
@@ -15,16 +15,20 @@ func AuthReverseSessionKey(userId int32) string {
 	return "auth:users:sessions:" + strconv.FormatInt(int64(userId), 10)
 }
 
-// RDBCacheMetricDataKey returns a RDB key for metrics.
-func RDBCacheMetricDataKey(metricId int64) string {
-	return "cache:metrics:" + strconv.FormatInt(metricId, 10)
-}
-
 // RDBCacheMetricIdContainerIdKey returns a RDB key for metric id and container id.
 func RDBCacheMetricIdContainerIdKey(teamIdent string, contextIdent string, metricIdent string) string {
-	return fmt.Sprintf("cache:metric_id:%s_%s_%s", teamIdent, contextIdent, metricIdent)
+	return fmt.Sprintf("cache:metrics:%s_%s_%s", teamIdent, contextIdent, metricIdent)
+}
+
+// RDBCacheMetricDataKey returns a RDB key for metrics.
+func RDBCacheMetricDataKey(metricId int64) string {
+	return fmt.Sprintf("cache:metrics:%d:full", metricId)
 }
 
 func RDBCacheMetricEvExpressionKey(metricId int64) string {
-	return fmt.Sprintf("cache:metrics_evexpression:%d", metricId)
+	return fmt.Sprintf("cache:metrics:%d:evexpression", metricId)
+}
+
+func RDBCacheMetricDataPolicyId(metricId int64) string {
+	return fmt.Sprintf("cache:metrics:%d:data-policy", metricId)
 }
