e8e9225049af0c76f1f9e9172ccb5d41669d5509
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-11-18 08:09:29 -0300

    feat(amqp): Add header handlers

--- shared/amqp/routing_key.go
+++ shared/amqp/routing_key.go
@@ -1,6 +1,11 @@
 package amqp
 
-import "github.com/fernandotsda/nemesys/shared/types"
+import (
+	"errors"
+
+	"github.com/fernandotsda/nemesys/shared/types"
+	"github.com/rabbitmq/amqp091-go"
+)
 
 func GetDataRoutingKey(t types.ContainerType) string {
 	switch t {
@@ -11,4 +16,16 @@ func GetDataRoutingKey(t types.ContainerType) string {
 	default:
 		return "snmp"
 	}
+}
+
+func RouteHeader(serviceName string) amqp091.Table {
+	return amqp091.Table{"routing_key": serviceName}
+}
+
+func GetRoutingKeyFromHeader(h amqp091.Table) (string, error) {
+	rk, ok := h["routing_key"].(string)
+	if !ok {
+		return "", errors.New("fail to make routing_key assertion from message header")
+	}
+	return rk, nil
 }
