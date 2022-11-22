67cbb4801d240fd5afeaf87e8f1a62e9ff40a263
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-11-22 10:42:56 -0300

    fix(task): Fix empty offset on task creation

--- shared/influxdb/aggregate.go
+++ shared/influxdb/aggregate.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/fernandotsda/nemesys/shared/models"
 	iapi "github.com/influxdata/influxdb-client-go/v2/api"
-	"github.com/influxdata/influxdb-client-go/v2/domain"
 )
 
 // getTaskName returns a task name for a data policy id.
@@ -14,28 +13,31 @@ func getTaskName(dataPolicyId int16) string {
 	return fmt.Sprintf("%d-aggr-task", dataPolicyId)
 }
 
-// createAggrTask creates an task of data aggregation.
-func (c *Client) createAggrTask(ctx context.Context, dp models.DataPolicy) (err error) {
-	api := c.TasksAPI()
-
-	every := fmt.Sprintf("%ds", dp.AggregationInterval)
-	flux := fmt.Sprintf(`
-			from(bucket: "%s")
-				|> range(start: -task.every)
-				|> aggregateWindow(every: %ds, fn: mean)
-				|> to(bucket: "%s")
-		`,
+// getTaskFlux returns the task flux.
+func getTaskFlux(dp models.DataPolicy) string {
+	return fmt.Sprintf(`
+		option task = {name: "%s", every: %s, offset: %s}
+		from(bucket: "%s")
+			|> range(start: -%s, stop: -%s)
+			|> filter(fn: (r) => r._measurement == "metrics")
+			|> aggregateWindow(every: %ds, fn: mean, createEmpty: false)
+			|> to(bucket: "%s")
+			`,
+		getTaskName(dp.Id),
+		fmt.Sprintf("%ds", dp.AggregationInterval),
+		fmt.Sprintf("%ds", dp.Retention*3600),
 		GetBucketName(dp.Id, false),
+		fmt.Sprintf("%ds", dp.Retention*3600),
+		fmt.Sprintf("%ds", dp.Retention*3600-dp.AggregationInterval),
 		dp.AggregationInterval,
 		GetBucketName(dp.Id, true),
 	)
+}
 
-	_, err = api.CreateTask(ctx, &domain.Task{
-		Flux:  flux,
-		Every: &every,
-		Name:  getTaskName(dp.Id),
-		OrgID: *c.DefaultOrg.Id,
-	})
+// createAggrTask creates an task of data aggregation.
+func (c *Client) createAggrTask(ctx context.Context, dp models.DataPolicy) (err error) {
+	api := c.TasksAPI()
+	_, err = api.CreateTaskByFlux(ctx, getTaskFlux(dp), *c.DefaultOrg.Id)
 	return err
 }
 
@@ -57,25 +59,7 @@ func (c *Client) updateAggrTask(ctx context.Context, dp models.DataPolicy) (err 
 		return ErrTaskNotFound
 	}
 	task := tasks[0]
-	every := fmt.Sprintf("%ds", dp.AggregationInterval)
-
-	// update params
-	task.Every = &every
-	task.Flux = fmt.Sprintf(`
-			option task = {name: "%s", every: %s}
-
-			from(bucket: "%s")
-				|> range(start: -task.every)
-				|> aggregateWindow(every: %ds, fn: mean)
-				|> to(bucket: "%s")
-		`,
-		getTaskName(dp.Id),
-		every,
-		GetBucketName(dp.Id, false),
-		dp.AggregationInterval,
-		GetBucketName(dp.Id, true),
-	)
-
+	task.Flux = getTaskFlux(dp)
 	_, err = api.UpdateTask(ctx, &task)
 	return err
 }
