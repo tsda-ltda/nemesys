d6eb3c0d63a343e6c696e0a0baae9f8cbd1e7671
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-11-29 10:46:09 -0300

    feat(logger): Add log fields

--- shared/logger/field.go
+++ shared/logger/field.go
@@ -9,3 +9,18 @@ func ErrField(err error) zapcore.Field {
 		Interface: err,
 	}
 }
+
+func Int64Field(key string, value int64) zapcore.Field {
+	return zapcore.Field{
+		Key:     key,
+		Type:    zapcore.Int64Type,
+		Integer: value,
+	}
+}
+func StringField(key string, value string) zapcore.Field {
+	return zapcore.Field{
+		Key:    key,
+		Type:   zapcore.StringType,
+		String: value,
+	}
+}
