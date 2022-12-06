85f0aadf9e1057039579992f3c6de2ab252000b3
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-12-06 11:16:55 -0300

    ref(api): Move get custom query handler from tools to avoid cycle import

--- api-manager/internal/contextual-metric/data.go
+++ api-manager/internal/contextual-metric/data.go
@@ -1,7 +1,9 @@
 package ctxmetric
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/fernandotsda/nemesys/api-manager/internal/api"
 	"github.com/fernandotsda/nemesys/api-manager/internal/tools"
@@ -11,6 +13,13 @@ import (
 	"github.com/fernandotsda/nemesys/shared/logger"
 	"github.com/fernandotsda/nemesys/shared/models"
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	ErrFailToGetCustomQueryOnCache    = errors.New("fail to get custom query on cache")
+	ErrFailToSetCustomQueryOnCache    = errors.New("fail to set custom query on cache")
+	ErrFailToGetCustomQueryOnDatabase = errors.New("fail to get custom query on database")
+	ErrCustomQueryNotFound            = errors.New("custom query does not exists")
 )
 
 // Retunrn the current metric's value.
@@ -81,12 +90,12 @@ func QueryDataHandler(api *api.API) func(c *gin.Context) {
 
 		opts.Start = c.Query("start")
 		opts.Stop = c.Query("stop")
-		cq, err := tools.GetCustomQueryFlux(api, c)
+		cq, err := GetCustomQueryFlux(api, c)
 		if err != nil {
 			if ctx.Err() != nil {
 				return
 			}
-			if err == tools.ErrCustomQueryNotFound {
+			if err == ErrCustomQueryNotFound {
 				c.JSON(http.StatusNotFound, tools.JSONMSG(tools.MsgCustomQueryNotFound))
 				return
 			}
@@ -110,4 +119,59 @@ func QueryDataHandler(api *api.API) func(c *gin.Context) {
 		}
 		c.JSON(http.StatusOK, points)
 	}
+}
+
+// GetCustomQueryFlux get the custom query id/ident on gin context query. Try to get
+// the flux on cache, if cache is missing goes to database and save on cache after.
+func GetCustomQueryFlux(api *api.API, c *gin.Context) (flux string, err error) {
+	ctx := c.Request.Context()
+	rawCustomQuery := c.Query("custom_query")
+	if len(rawCustomQuery) != 0 {
+		id, err := strconv.ParseInt(rawCustomQuery, 0, 32)
+		if err != nil {
+			cacheRes, err := api.Cache.GetCustomQueryByIdent(ctx, rawCustomQuery)
+			if err != nil {
+				return flux, ErrFailToGetCustomQueryOnCache
+			}
+			if !cacheRes.Exists {
+				dbRes, err := api.PG.GetCustomQueryByIdent(ctx, rawCustomQuery)
+				if err != nil {
+					return flux, ErrFailToGetCustomQueryOnDatabase
+				}
+				if !dbRes.Exists {
+					return flux, ErrCustomQueryNotFound
+				}
+				flux = dbRes.CustomQuery.Flux
+				err = api.Cache.SetCustomQueryByIdent(ctx, dbRes.CustomQuery.Flux, rawCustomQuery)
+				if err != nil {
+					return flux, ErrFailToSetCustomQueryOnCache
+				}
+			} else {
+				flux = cacheRes.Flux
+			}
+		} else {
+			cacheRes, err := api.Cache.GetCustomQuery(ctx, int32(id))
+			if err != nil {
+				return flux, ErrFailToGetCustomQueryOnCache
+			}
+
+			if !cacheRes.Exists {
+				dbRes, err := api.PG.GetCustomQuery(ctx, int32(id))
+				if err != nil {
+					return flux, ErrFailToGetCustomQueryOnDatabase
+				}
+				if !dbRes.Exists {
+					return flux, ErrCustomQueryNotFound
+				}
+				flux = dbRes.CustomQuery.Flux
+				err = api.Cache.SetCustomQuery(ctx, dbRes.CustomQuery.Flux, int32(id))
+				if err != nil {
+					return flux, ErrFailToSetCustomQueryOnCache
+				}
+			} else {
+				flux = cacheRes.Flux
+			}
+		}
+	}
+	return flux, nil
 }
