4b4939e693bb3fc7b8fda8dc4174df736fc7fa13
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-12-07 13:41:46 -0300

    ref(models): Update models

--- shared/models/alarm.go
+++ shared/models/alarm.go
@@ -1,61 +1,32 @@
 package models
 
-import (
-	"github.com/fernandotsda/nemesys/shared/types"
-)
-
-type AlarmExpression struct {
-	// MetricId is the metric identifier.
-	MetricId int64 `json:"metric-id" validate:"-"`
-	// MinorExpression is the minor expression.
-	MinorExpression string `json:"minor-expression" validate:"max=255"`
-	// MajorExpression is the major expression.
-	MajorExpression string `json:"major-expression" validate:"max=255"`
-	// CriticalExpression is the critical expression.
-	CriticalExpression string `json:"critical-expression" validate:"max=255"`
-	// MinorDescr is the description of the minor alarm state. This description
-	// is sent in the alarm notification to help the user to undestand the
-	// alarm context.
-	MinorDescr string `json:"minor-descr" validate:"max=255"`
-	// MajorDescr is the description of the major alarm state. This description
-	// is sent in the alarm notification to help the user to undestand the
-	// alarm context.
-	MajorDescr string `json:"major-descr" validate:"max=255"`
-	// CriticalDescr is the description of the critical alarm state. This description
-	// is sent in the alarm notification to help the user to undestand the
-	// alarm context.
-	CriticalDescr string `json:"critical-descr" validate:"max=255"`
-}
-
 type AlarmProfile struct {
 	// Id is the alarm profile unique identifier.
 	Id int32 `json:"id" validate:"-"`
 	// Name is the alarm profile name.
 	Name string `json:"name" validate:"required,max=50"`
-	// Minor is if the alarm profile wants to receive minor alarms.
-	Minor bool `json:"minor" validate:"-"`
-	// Major is if the alarm profile wants to receive major alarms.
-	Major bool `json:"major" validate:"-"`
-	// Critical is if the alarm profile wants to receive critical alarms.
-	Critical bool `json:"critical" validate:"-"`
+	// Descr is the alarm profile description.
+	Descr string `json:"descr" validate:"required,max=255"`
 }
 
-type AlarmState struct {
-	// MetricId is the metric identifier.
-	MetricId int64 `json:"metric-id"`
-	// State is the current metric state.
-	State types.AlarmState `json:"state"`
-	// LastMinorTime is the last minor alarm occurency in milliseconds.
-	LastMinorTime int64 `json:"last-minor-time"`
-	// LastMajorTime is the last major alarm occurency in milliseconds.
-	LastMajorTime int64 `json:"last-major-time"`
-	// LastMinorTime is the last critical alarm occurency in milliseconds.
-	LastCriticalTime int64 `json:"last-critical-time"`
-	// LastRecognizationTime is when the metric was recognized.
-	LastRecognizationTime int64 `json:"last-recognization-time"`
-	// AlwaysAlarmedOnNewAlarm if setted to true will set the state as
-	// alarmed every new alarm, ignoring the recognization.
-	AlwaysAlarmedOnNewAlarm bool `json:"always-alarmed-on-new-alarm"`
-	// RecognizationLifetime is the recognization lifetime in seconds.
-	RecognizationLifetime int64 `json:"recognization-lifetime"`
+type AlarmCategory struct {
+	// Id is the alarm category unique identifier.
+	Id int32 `json:"id" validate:"-"`
+	// Name is the alarm category name.
+	Name string `json:"name" validate:"required,max=50"`
+	// Descr is the alarm category description.
+	Descr string `json:"descr" validate:"required,max=255"`
+	// Lever is the alarm level.
+	Level int32 `json:"level" validate:"-"`
+}
+
+type AlarmExpression struct {
+	// Id is the alarm expression unique identifier.
+	Id int32 `json:"id" validate:"-"`
+	// Name is the alarm expression name.
+	Name string `json:"name" validate:"required,max=50"`
+	// Expression is the alarm expression.
+	Expression string `json:"expression" validate:"required,max=255"`
+	// AlarmCategoryId is the alarm category id.
+	AlarmCategoryId int32 `json:"alarm-category-id" validate:"required"`
 }
