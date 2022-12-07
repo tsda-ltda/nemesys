ea5bfbb3b6629d4024c0cfc69d273a73e642afd5
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-12-07 13:37:05 -0300

    ref(types): Update alarm types

--- shared/types/alarm.go
+++ shared/types/alarm.go
@@ -1,11 +1,25 @@
 package types
 
 type AlarmState int16
+type AlarmType uint8
 
 const (
 	ASNotAlarmed AlarmState = iota
+	ASAlarmed
 	ASMinorAlarm
 	ASMajorAlarm
 	ASCriticalAlarm
 	ASMAlarmRecognized
 )
+
+const (
+	ATNotAlarmed AlarmType = iota
+	ATAlarmed
+	ATMinorAlarm
+	ATMajorAlarm
+	ATCriticalAlarm
+)
+
+func IsAlarmed(at AlarmType) bool {
+	return at > ATNotAlarmed
+}
