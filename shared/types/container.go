8a29ba49935026c5a2bd5356f43950c4f8f7eb4d
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-12-09 10:57:11 -0300

    feat(types): Add alarm types

--- shared/types/container.go
+++ shared/types/container.go
@@ -10,5 +10,18 @@ const (
 )
 
 func IsNonFlex(ct ContainerType) bool {
-	return ct == CTFlexLegacy
+	return ct != CTFlexLegacy
+}
+
+func StringfyContainerType(t ContainerType) string {
+	switch t {
+	case CTBasic:
+		return "Basic"
+	case CTFlexLegacy:
+		return "Flex Legacy"
+	case CTSNMPv2c:
+		return "SNMPv2c"
+	default:
+		return "Unknown"
+	}
 }
