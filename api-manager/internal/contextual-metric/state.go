2fc822e5d06f184d54afa1ac7e225133c009095c
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2022-12-12 15:16:34 -0300

    feat(api): Add alarm state routes

---
+++ api-manager/internal/contextual-metric/state.go
@@ -0,0 +1,150 @@
+package ctxmetric
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/fernandotsda/nemesys/api-manager/internal/api"
+	"github.com/fernandotsda/nemesys/api-manager/internal/tools"
+	"github.com/fernandotsda/nemesys/shared/logger"
+	"github.com/fernandotsda/nemesys/shared/types"
+	"github.com/gin-gonic/gin"
+)
+
+// Get the metric's alarm state.
+// Responses:
+//   - 404 If alarm state not found.
+//   - 200 If succeeded.
+func GetAlarmStateHandler(api *api.API) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+
+		rawId := c.Param("ctxMetricId")
+		id, err := strconv.ParseInt(rawId, 0, 64)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, tools.JSONMSG(tools.MsgInvalidParams))
+			return
+		}
+
+		exists, state, err := api.PG.GetAlarmStateByCtxId(ctx, id)
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+			c.Status(http.StatusInternalServerError)
+			api.Log.Error("Fail to get alarm state", logger.ErrField(err))
+			return
+		}
+		if !exists {
+			c.JSON(http.StatusNotFound, tools.JSONMSG(tools.MsgAlarmStateNotFound))
+			return
+		}
+
+		c.JSON(http.StatusOK, state)
+	}
+}
+
+// Updates the metric's alarm state to recognized.
+// Responses:
+//   - 404 If alarm state not found.
+//   - 400 If alarm state is not in alarm state.
+//   - 200 If succeeded.
+func RecognizeAlarmStateHandler(api *api.API) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+
+		rawId := c.Param("ctxMetricId")
+		id, err := strconv.ParseInt(rawId, 0, 64)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, tools.JSONMSG(tools.MsgInvalidParams))
+			return
+		}
+
+		exists, state, err := api.PG.GetAlarmStateByCtxId(ctx, id)
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+			c.Status(http.StatusInternalServerError)
+			api.Log.Error("Fail to get alarm state", logger.ErrField(err))
+			return
+		}
+		if !exists {
+			c.JSON(http.StatusNotFound, tools.JSONMSG(tools.MsgAlarmStateNotFound))
+			return
+		}
+
+		if state.State != types.ASAlarmed {
+			c.JSON(http.StatusBadRequest, tools.JSONMSG(tools.MsgMetricIsNotAlarmed))
+			return
+		}
+
+		state.LastUpdate = time.Now().Unix()
+		state.State = types.ASRecognized
+
+		_, err = api.PG.UpdateAlarmState(ctx, state)
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+			c.Status(http.StatusInternalServerError)
+			api.Log.Error("Fail to update alarm state", logger.ErrField(err))
+			return
+		}
+
+		c.Status(http.StatusOK)
+	}
+}
+
+// Updates the metric's alarm state to not alarmed.
+// Responses:
+//   - 404 If alarm state not found.
+//   - 400 If alarm state is not in recognized state.
+//   - 200 If succeeded.
+func ResolveAlarmStateHandler(api *api.API) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+
+		rawId := c.Param("ctxMetricId")
+		id, err := strconv.ParseInt(rawId, 0, 64)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, tools.JSONMSG(tools.MsgInvalidParams))
+			return
+		}
+
+		exists, state, err := api.PG.GetAlarmStateByCtxId(ctx, id)
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+			c.Status(http.StatusInternalServerError)
+			api.Log.Error("Fail to get alarm state", logger.ErrField(err))
+			return
+		}
+		if !exists {
+			c.JSON(http.StatusNotFound, tools.JSONMSG(tools.MsgAlarmStateNotFound))
+			return
+		}
+
+		if state.State != types.ASRecognized {
+			c.JSON(http.StatusBadRequest, tools.JSONMSG(tools.MsgMetricIsNotRecognized))
+			return
+		}
+
+		state.LastUpdate = time.Now().Unix()
+		state.State = types.ASNotAlarmed
+
+		_, err = api.PG.UpdateAlarmState(ctx, state)
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+			c.Status(http.StatusInternalServerError)
+			api.Log.Error("Fail to update alarm state", logger.ErrField(err))
+			return
+		}
+
+		c.Status(http.StatusOK)
+	}
+}
--- shared/pg/alarm_state.go
+++ shared/pg/alarm_state.go
@@ -8,9 +8,11 @@ import (
 )
 
 const (
-	sqlAlarmStateCreate = `INSERT INTO alarm_state (metric_id, state, last_update) VALUES($1, $2, $3);`
-	sqlAlarmStateUpdate = `UPDATE alarm_state SET (state, last_update) = ($1, $2) WHERE metric_id = $3;`
-	sqlAlarmStateGet    = `SELECT state, last_update FROM alarm_state WHERE metric_id = $1;`
+	sqlAlarmStateCreate     = `INSERT INTO alarm_state (metric_id, state, last_update) VALUES($1, $2, $3);`
+	sqlAlarmStateUpdate     = `UPDATE alarm_state SET (state, last_update) = ($1, $2) WHERE metric_id = $3;`
+	sqlAlarmStateGet        = `SELECT state, last_update FROM alarm_state WHERE metric_id = $1;`
+	sqlAlarmStateGetByCtxId = `SELECT a.metric_id, a.state, a.last_update FROM alarm_state a 
+		LEFT JOIN contextual_metrics cm ON cm.metric_id = a.metric_id WHERE cm.id = $1;`
 )
 
 func (pg *PG) CreateAlarmState(ctx context.Context, state models.AlarmState) (err error) {
@@ -36,5 +38,16 @@ func (pg *PG) GetAlarmState(ctx context.Context, metricId int64) (exists bool, s
 		return false, state, err
 	}
 	state.MetricId = metricId
+	return true, state, nil
+}
+
+func (pg *PG) GetAlarmStateByCtxId(ctx context.Context, ctxMetricId int64) (exists bool, state models.AlarmState, err error) {
+	err = pg.db.QueryRowContext(ctx, sqlAlarmStateGetByCtxId, ctxMetricId).Scan(&state.MetricId, &state.State, &state.LastUpdate)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, state, nil
+		}
+		return false, state, err
+	}
 	return true, state, nil
 }
